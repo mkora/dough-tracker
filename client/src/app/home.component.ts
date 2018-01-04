40f94e9489ddb9c5967422e877f8eccbc0ea1cc5
mkor <31717889+mkora@users.noreply.github.com>
2018-01-04 16:09:59 -0500

    Updated home component

--- client/src/app/home.component.ts
+++ client/src/app/home.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { APP_CONFIG, AppConfig } from './app-config.module';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  //styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  currentYear: number;
+
+  constructor(
+    @Inject(APP_CONFIG) private config: AppConfig
+  ) { }
 
   ngOnInit() {
+    this.currentYear = this.config.dataCurrentYear;
   }
 
 }
