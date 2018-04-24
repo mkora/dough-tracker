728f7350fcaeacc0f2f5f2c1405f340afe28dac5
mkor <31717889+mkora@users.noreply.github.com>
2018-04-23 20:46:11 -0400

    Update library code style

--- src/libs/Budget/Statement/Data/Transaction.php
+++ src/libs/Budget/Statement/Data/Transaction.php
@@ -2,231 +2,285 @@
 
 namespace Budget\Statement\Data;
 
-/**
- * Class for storing data
- *
- * @package    Finance
- * @author
-*/
 class Transaction
 {
 
-	/**
-  * Month, for easy quering
-	*
-  * @param int
-  */
-	private $month;
+    /**
+     * Month
+     *
+     * @var int
+     */
+    protected $month;
 
-	/**
-  * Year, for easy quering
-	*
-  * @param int
-  */
-	private $year;
+    /**
+     * Year
+     *
+     * @var int
+     */
+    protected $year;
 
-	/**
-  *
-  * @param string
-  */
-	private $date;
+    /**
+     * Full date
+     * 
+     * @var string
+     */
+    protected $date;
 
-	/**
-  * Debit or credit [-1, 1]
-	*
-  * @param int
-  */
-	private $type;
+    /**
+     * Debit or credit [-1, 1]
+     *
+     * @var int
+     */
+    protected $type;
 
-	/**
-  * Assigned category alias
-	*
-  * @param string
-  */
-	private $category;
+    /**
+     * Assigned category alias
+     *
+     * @var string
+     */
+    protected $category;
 
-	/**
-  * Description
-	*
-  * @param string
-  */
-	private $title;
+    /**
+     * Description
+     *
+     * @var string
+     */
+    protected $title;
 
-	/**
-  * Amount
-	*
-  * @param float
-  */
-	private $sum;
+    /**
+     * Amount
+     *
+     * @var float
+     */
+    protected $sum;
 
+    /**
+     * Constructor
+     *
+     * @param array $a data
+     */
+    public function __construct(array $a = array())
+    {
+        if (!empty($a)) {
+            $this->fromArray($a);
+        }
+    }
 
-	public function __construct(array $a = array())
-	{
-		if (!empty($a))
-			$this->fromArray($a);
-	}
+    /**
+     * Initializes an object from an array
+     *
+     * @param array $a data
+     *
+     * @return void
+     */
+    public function fromArray(array $a)
+    {
+        if (isset($a['month'])) {
+            $this->month = $a['month'];
+        }
+        if (isset($a['year'])) {
+            $this->year = $a['year'];
+        }
+        if (isset($a['date'])) {
+            $this->date = $a['date'];
+        }
+        if (isset($a['title'])) {
+            $this->title = $a['title'];
+        }
+        if (isset($a['sum'])) {
+            $this->sum = $a['sum'];
+        }
+        if (isset($a['type'])) {
+            $this->type = $a['type'];
+        }
+        if (isset($a['category'])) {
+            $this->category = $a['category'];
+        }
+    }
 
-	/**
-  * Initializes an object from an array
-  *
-  * @param array $a
-  * @return void
-  */
-	public function fromArray(array $a)
-	{
-		if (isset($a['month']))
-			$this->month = $a['month'];
-		if (isset($a['year']))
-			$this->year = $a['year'];
-		if (isset($a['date']))
-			$this->date = $a['date'];
-		if (isset($a['title']))
-			$this->title = $a['title'];
-		if (isset($a['sum']))
-			$this->sum = $a['sum'];
-		if (isset($a['type']))
-			$this->type = $a['type'];
-		if (isset($a['category']))
-			$this->category = $a['category'];
-	}
+    /**
+     * Returns an object as an array
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        $a = array();
+        if ($this->month !== null) {
+            $a['month'] = $this->month;
+        }
+        if ($this->year !== null) {
+            $a['year'] = $this->year;
+        }
+        if ($this->date !== null) {
+            $a['date'] = $this->date;
+        }
+        if ($this->type !== null) {
+            $a['type'] = $this->type;
+        }
+        if ($this->category !== null) {
+            $a['category'] = $this->category;
+        }
+        if ($this->title !== null) {
+            $a['title'] = $this->title;
+        }
+        if ($this->sum !== null) {
+            $a['sum'] = $this->sum;
+        }
+        return $a;
+    }
 
-	/**
-  * Returns an object as an array
-  *
-  * @param void
-  * @return array
-  */
-	public function toArray()
-	{
-		$a = array();
-		if ($this->month !== NULL)
-			$a['month'] = $this->month;
-		if ($this->year !== NULL)
-			$a['year'] = $this->year;
-		if ($this->date !== NULL)
-			$a['date'] = $this->date;
-		if ($this->type !== NULL)
-			$a['type'] = $this->type;
-		if ($this->category !== NULL)
-			$a['category'] = $this->category;
-		if ($this->title !== NULL)
-			$a['title'] = $this->title;
-		if ($this->sum !== NULL)
-			$a['sum'] = $this->sum;
-		return $a;
-	}
+    /**
+     * Sets a category
+     *
+     * @param string $category category
+     *
+     * @return void
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+    }
 
-	/**
-	 * @param string $category
-	 */
-	public function setCategory($category)
-	{
-		$this->category = $category;
-	}
+    /**
+     * Returns a category
+     *
+     * @return string
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getCategory()
-	{
-		return $this->category;
-	}
+    /**
+     * Sets a date
+     *
+     * @param string $date timestamp
+     *
+     * @return void
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
 
-	/**
-	 * @param string $date
-	 */
-	public function setDate($date)
-	{
-		$this->date = $date;
-	}
+    /**
+     * Returns a date
+     *
+     * @return string
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getDate()
-	{
-		return $this->date;
-	}
+    /**
+     * Sets a month
+     *
+     * @param int $month month
+     *
+     * @return void
+     */
+    public function setMonth($month)
+    {
+        $this->month = $month;
+    }
 
-	/**
-	 * @param int $month
-	 */
-	public function setMonth($month)
-	{
-		$this->month = $month;
-	}
+    /**
+     * Returns a month
+     *
+     * @return int
+     */
+    public function getMonth()
+    {
+        return $this->month;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getMonth()
-	{
-		return $this->month;
-	}
+    /**
+     * Sets an amount
+     *
+     * @param float $sum amount
+     *
+     * @return void
+     */
+    public function setSum($sum)
+    {
+        $this->sum = $sum;
+    }
 
-	/**
-	 * @param float $sum
-	 */
-	public function setSum($sum)
-	{
-		$this->sum = $sum;
-	}
+    /**
+     * Returns an amount
+     *
+     * @return float
+     */
+    public function getSum()
+    {
+        return $this->sum;
+    }
 
-	/**
-	 * @return float
-	 */
-	public function getSum()
-	{
-		return $this->sum;
-	}
+    /**
+     * Sets a title
+     *
+     * @param string $title title
+     *
+     * @return void
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
 
-	/**
-	 * @param string $title
-	 */
-	public function setTitle($title)
-	{
-		$this->title = $title;
-	}
+    /**
+     * Returns a title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getTitle()
-	{
-		return $this->title;
-	}
+    /**
+     * Sets a type: credit/debet
+     *
+     * @param int $type -1/1
+     *
+     * @return void
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
 
-	/**
-	 * @param int $type
-	 */
-	public function setType($type)
-	{
-		$this->type = $type;
-	}
+    /**
+     * Returns a type
+     *
+     * @return int
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getType()
-	{
-		return $this->type;
-	}
+    /**
+     * Sets an year
+     *
+     * @param int $year year
+     *
+     * @return void
+     */
+    public function setYear($year)
+    {
+        $this->year = $year;
+    }
 
-	/**
-	 * @param int $year
-	 */
-	public function setYear($year)
-	{
-		$this->year = $year;
-	}
-
-	/**
-	 * @return int
-	 */
-	public function getYear()
-	{
-		return $this->year;
-	}
-
-
+    /**
+     * Returns an year
+     *
+     * @return int
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
 }
--- src/libs/Budget/Statement/ServiceFactory.php
+++ src/libs/Budget/Statement/ServiceFactory.php
@@ -3,75 +3,91 @@
 namespace Budget\Statement;
 
 use Budget\Statement\Service\{
-  ServiceAbstract,
-  ServiceExeption,
-  BOA,
-  Citizen,
-  Discover
+    ServiceAbstract,
+    ServiceExeption,
+    BOA,
+    Citizen,
+    Discover
 };
 
-/**
- * Generates an instance of a class from the given type
- *
- * @package    Finance
- * @author
-*/
 class ServiceFactory
 {
-  /**
-  * type is Bank of America statement (by prefix)
-  * @param string
-  */
-  const STATEMENT_BOA = 'boa';
+    /**
+     * Type is Bank of America statement
+     * (by prefix)
+     *
+     * @var string
+     */
+    const STATEMENT_BOA = 'boa';
 
-  /**
-  * type is Citizen statement (by prefix)
-  * @param string
-  */
-  const STATEMENT_CITIZEN = 'cit';
+    /**
+     * Type is Citizen statement
+     * (by prefix)
+     *
+     * @var string
+     */
+    const STATEMENT_CITIZEN = 'cit';
 
-  /**
-  * type is Discover statement (by prefix)
-  * @param string
-  */
-  const STATEMENT_DISCOVER = 'dis';
+    /**
+     * Type is Discover statement
+     * (by prefix)
+     *
+     * @var string
+     */
+    const STATEMENT_DISCOVER = 'dis';
 
+    /**
+     * Array of statement types
+     *
+     * @static
+     *
+     * @var array
+     */
+    protected static $statementTypes = array(
+        self::STATEMENT_BOA,
+        self::STATEMENT_CITIZEN,
+        self::STATEMENT_DISCOVER,
+    );
 
-  private static $statementTypes = array(
-    self::STATEMENT_BOA,
-    self::STATEMENT_CITIZEN,
-    self::STATEMENT_DISCOVER,
-  );
+    /**
+     * Checks if type of a statement
+     * belongs to existed ones
+     *
+     * @param string $type type
+     *
+     * @static
+     * @return bool
+     */
+    public static function hasType($type): bool
+    {
+        return in_array($type, self::$statementTypes);
+    }
 
-  /**
-  * If type of a statement belongs to existed ones
-  *
-  * @static
-  * @param string $type
-  * @return bool
-  */
-  public static function hasType($type) : bool
-  {
-    return in_array($type, self::$statementTypes);
-  }
-
-  /**
-  * Returns an instance of a class which would parse a statement line
-  *
-  * @static
-  * @throws ErrorException
-  * @param string $type
-  * @return SIntarface
-  */
-  public static function getInstance($type) : ServiceAbstract
-  {
-    if (!self::hasType($type))
-      throw new \ErrorException("Can't create an object. Given type doesn't exists");
-
-    if ($type == self::STATEMENT_BOA) return new BOA();
-    if ($type == self::STATEMENT_CITIZEN) return new Citizen();
-    if ($type == self::STATEMENT_DISCOVER) return new Discover();
-
-  }
-
+    /**
+     * Returns an instance of a class
+     * which is responsible of parsing
+     * a statement line by line
+     *
+     * @param string $type type
+     *
+     * @static
+     * @throws ErrorException
+     * @return SIntarface
+     */
+    public static function getInstance($type): ServiceAbstract
+    {
+        if (!self::hasType($type)) {
+            $msg = "Can't create an object. Given type doesn't exists";
+            throw new \ErrorException($msg);
+        }
+        if ($type == self::STATEMENT_BOA) {
+            return new BOA();
+        }
+        if ($type == self::STATEMENT_CITIZEN) {
+            return new Citizen();
+        }
+        if ($type == self::STATEMENT_DISCOVER) {
+            return new Discover();
+        }
+    }
 }
